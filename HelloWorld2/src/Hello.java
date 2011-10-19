bf4fa63ac680dda6b4f36153c85273261b9e2364
chinajimyang <oraclebone@gmail.com>
2011-10-19 10:43:32 +0800

    Third Change on Hello.java

--- HelloWorld2/src/Hello.java
+++ HelloWorld2/src/Hello.java
@@ -8,6 +8,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Hello World2");
+		System.out.println("Hello World3");
 	}
 
 }
